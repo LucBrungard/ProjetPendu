12f7d981038e0abaab2c7495b6dc8f93d5a55971
LucBrungard <luc.brungard@laposte.net>
2022-10-27 08:31:01 +0200

    Add base for client and server

---
+++ src/server/Server.java
@@ -0,0 +1,13 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server extends ServerSocket {
+
+    public Server() throws IOException {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+    
+}
