16994b90cbdd7cfd3b33e9515020b5869a349eba
LucBrungard <luc.brungard@laposte.net>
2022-11-09 13:14:30 +0100

    Implement states of the client and the game

---
+++ server/src/game/ClientState.java
@@ -0,0 +1,8 @@
+package game;
+
+public enum ClientState {
+    IN_GAME,
+    IN_MENU_PROGRESS,
+    IN_MENU_ERROR,
+    DISCONNECTED;
+}
--- server/src/game/GameState.java
+++ server/src/game/GameState.java
@@ -1,10 +1,8 @@
 package game;
 
 public enum GameState {
+    RUNNING,
     WON,
     LOST,
-    STOP,
-    IN_GAME,
-    IN_MENU_PROGRESS,
-    IN_MENU_ERROR;
+    STOPPED;
 }
