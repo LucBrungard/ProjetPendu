7a1ded38b1ba3fb9a97a84ad1a662e68d8046cf3
LucBrungard <luc.brungard@laposte.net>
2022-11-09 13:14:54 +0100

    Refactor : add game modes

--- server/src/game/Game.java
+++ server/src/game/Game.java
@@ -3,35 +3,75 @@ package game;
 import java.util.HashSet;
 import java.util.Set;
 
-import game.settings.Difficulty;
 import game.settings.WordsDictionnary;
 
-public class Game {
-    private Difficulty difficulty = Difficulty.NORMAL;
-    private String toFind;
-    private String currentStateWord = "";
-    private int errors = 0;
-    private GameState gameState;
-    private Set<String> alreadyProposed = new HashSet<String>();
+public abstract class Game {
+    /** The word to find */
+    protected String toFind;
 
-    public Game() {}
+    /** The current state of the guessing of the client */
+    protected String currentStateWord;
 
+    /** The number of errors made by the client */
+    protected int errors = 0;
+
+    /** THe max errors a client can make */
+    protected int maxErrors;
+
+    /** The state of the game. See {@link game.GameState} */
+    protected GameState gameState;
+
+    /** The letters already proposed by the client */
+    protected Set<String> alreadyProposed = new HashSet<String>();
+
+    /** The settings of a game */
+    protected GameSetting settings;
+
+    public Game(GameSetting settings) {
+        if (settings != null) this.settings = settings;
+    }
+
+    
+    /**
+     * Initialize the variables to start a new game
+     */
     public void start() {
         this.errors = 0;
+        this.maxErrors = this.settings.getDifficulty().maxErrors;
+
+        this.toFind = WordsDictionnary.getRandomWord(this.settings.getDifficulty());
         this.currentStateWord = "";
-        this.gameState = GameState.IN_GAME;
-        this.alreadyProposed.clear();
-        this.toFind = WordsDictionnary.getRandomWord(this.difficulty);
         for (int i = 0; i < toFind.length(); i++) {
             currentStateWord = currentStateWord.concat("_ ");
         }
+
+        this.alreadyProposed.clear();
+
+        this.gameState = GameState.RUNNING;
+
+        this.startSpecialRule();
     }
 
+
+    /** Add special cases when playing modes */
+    public abstract void startSpecialRule();
+
+
+    /**
+     * Check if a letter has already been proposed
+     * @param character The letter to check
+     * @return True if not already proposed, false otherwise
+     */
     public boolean validLetter(char character) {
         String charAsString = Character.toString(Character.toLowerCase(character));
         return this.alreadyProposed.add(charAsString);
     }
 
+
+    /**
+     * Check if a letter is in the word to find and update the currentStateWord
+     * @param character The letter to check
+     */
     public void guessLetter(char character) {
         String charAsString = Character.toString(Character.toLowerCase(character));
 
@@ -40,6 +80,7 @@ public class Game {
 
         if (idx == -1) {
             this.errors += 1;
+            if (this.hasLost()) this.gameState = GameState.LOST;
             return;
         }
 
@@ -49,21 +90,41 @@ public class Game {
         } while (idx != -1);
 
         this.currentStateWord = tmp.toString();
+
+        if (this.hasBeenFound()) this.gameState = GameState.WON;
     }
 
-    public boolean hasBeenFound() {
+
+    /**
+     * Check if the word to find is the word proposed
+     * @param word The word to check
+     * @return True if the word is good, false otherwise
+     */
+    public void guessWordToFind(String word) {
+        boolean res = this.toFind.equalsIgnoreCase(word);
+        if (!res) {
+            this.errors += 1;
+        } else {
+            this.gameState = GameState.WON;
+        }
+    }
+
+
+    /**
+     * Check if the word has been found
+     * @return True if it has been found, false otherwise
+     */
+    private boolean hasBeenFound() {
         return this.currentStateWord.replaceAll(" ", "").equals(this.toFind);
     }
 
-    public boolean hasLost() {
-        return this.errors == this.difficulty.maxErrors;
-    }
-
-    public boolean guessWordToFind(String word) {
-        boolean res = this.toFind.equalsIgnoreCase(word);
-        if (!res) this.errors += 1;
-
-        return res;
+    
+    /**
+     * Check if the game is lost
+     * @return True if the game is lost, false otherwise
+     */
+    private boolean hasLost() {
+        return this.errors == this.maxErrors;
     }
 
     ////////////////////////////////////////////////////////////////////
@@ -81,10 +142,6 @@ public class Game {
         return this.currentStateWord;
     }
 
-    public Difficulty getDifficulty() {
-        return this.difficulty;
-    }
-
     public static String getForfeitCommand() {
         return "/ff";
     }
@@ -94,30 +151,34 @@ public class Game {
     }
     
     public int getMaxErrors() {
-        return this.difficulty.maxErrors;
+        return this.maxErrors;
     }
     
     public String getToFind() {
         return this.toFind;
     }
+
+    /** Returns the description of this mode */
+    public abstract String getDescription();
     
     ////////////////////////////////////////////////////////////////////
     //////////////////             SETTERS             /////////////////
     ////////////////////////////////////////////////////////////////////
-    public void setDifficulty(Difficulty difficulty) {
-        this.difficulty = difficulty;
-    }
-
     public void setState(GameState gameState) {
         if (gameState != null) {
             this.gameState = gameState;
         }
     }
 
+    public void setMaxErrors(int value) {
+        this.maxErrors = value;
+    }
+
+
     @Override
     public String toString() {
-        return "Game [difficulty=" + difficulty + ", toFind=" + toFind + ", currentStateWord=" + currentStateWord
-                + ", maxErrors=" + difficulty.maxErrors + ", errors=" + errors + ", gameState=" + gameState + ", alreadyProposed="
-                + alreadyProposed + "]";
+        return "Game [toFind=" + toFind + ", currentStateWord=" + currentStateWord + ", errors=" + errors
+                + ", maxErrors=" + maxErrors + ", gameState=" + gameState + ", alreadyProposed=" + alreadyProposed
+                + ", settings=" + settings + "]";
     }
 }
