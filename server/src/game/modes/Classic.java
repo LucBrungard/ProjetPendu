73e6d7df9a6ba77cfecfb325093d4549c0354631
LucBrungard <luc.brungard@laposte.net>
2022-11-10 16:38:32 +0100

    refactor : make caca code

--- server/src/game/modes/Classic.java
+++ server/src/game/modes/Classic.java
@@ -11,16 +11,20 @@ public class Classic extends Game {
     @Override
     public void startSpecialRule() {}
 
-    @Override
-    public String getDescription() {
-        String res = "C'est le mode classique du jeu de pendu : ";
-        res = res.concat("\n\t- On vous donne un mot");
-        res = res.concat("\n\t- Proposez une lettre ou un mot");
-        res = res.concat("\n\t- Si vous vous êtes trompé dans votre proposition, votre compte d'erreur augmente");
-        res = res.concat("\n\t- Lorsque vous arrivez au maximum d'erreurs possibles, vous perdez la partie");
-        res = res.concat("\n\t- Si vous trouvez le mot avant de dépasser le nombre d'erreurs maximales, vous ganez la partie");
-        res = res.concat("\nBonne chance");
+    public static String getFullDescription() {
+        String res = "===================================\n";
+        res = res.concat("=========== CLASSIQUE =============\n");
+        res = res.concat("===================================\n\n");
 
+        res = res.concat("Limite de temps : aucune\n");
+        res = res.concat("Nombre d'essais : En fonction de la difficulté\n\n");
+
+        res = res.concat("Détails : C'est le mode classique du jeu de pendu :\n");
+        res = res.concat("\t- On vous donne un mot\n");
+        res = res.concat("\t- Si vous vous êtes trompé dans votre proposition, votre compte d'erreur augmente\n");
+        res = res.concat("\t- Lorsque vous arrivez au maximum d'erreurs possibles, vous perdez la partie\n");
+        res = res.concat("\t- Si vous trouvez le mot avant de dépasser le nombre d'erreurs maximales, vous ganez la partie\n");
+        res = res.concat("Refléchissez bien avant de jouer !\n");
         return res;
     }
 }
